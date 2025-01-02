6c4d46252040dfb64fb5f4d64a8ee30516652935
DanielEscobar21 <danesc21@gmail.com>
2025-01-02 16:33:31 -0600

    fix/next

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portafolio',
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.optimization.minimize = false;
-    return config;
-  },
-  experimental: {
-    optimizeCss: false
-  }
 }
 
 module.exports = nextConfig 
